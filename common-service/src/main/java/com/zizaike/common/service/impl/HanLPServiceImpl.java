8ae8e43e43b464344ec17ad2796366ebab6cb498
snowzhang <snowzhang@zizaike.com>
2015-12-02 11:28:33 +0800

    优化pom.xml

--- common-service/src/main/java/com/zizaike/common/service/impl/HanLPServiceImpl.java
+++ common-service/src/main/java/com/zizaike/common/service/impl/HanLPServiceImpl.java
@@ -9,28 +9,14 @@
 
 package com.zizaike.common.service.impl;
 
-import java.lang.reflect.Field;
-import java.util.AbstractList;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
-import com.alibaba.dubbo.common.json.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.hankcs.hanlp.HanLP;
-import com.zizaike.core.common.ranslator.Translate;
-import com.zizaike.entity.recommend.DestConfig;
-import com.zizaike.entity.recommend.Loctype;
-import com.zizaike.entity.recommend.Recommend;
-import com.zizaike.entity.recommend.vo.CountryArea;
-import com.zizaike.entity.recommend.vo.RecommendArea;
 import com.zizaike.is.common.HanLPService;
 
 /**
  * ClassName:HanLPServiceImpl <br/>
- * Function: TODO ADD FUNCTION. <br/>
- * Reason: TODO ADD REASON. <br/>
+ * Function: HanLP 简繁转换. <br/>
  * Date: 2015年11月20日 下午8:59:42 <br/>
  * 
  * @author snow.zhang
@@ -50,19 +36,5 @@ public class HanLPServiceImpl implements HanLPService {
           
         return HanLP.convertToSimplifiedChinese(str);
     }
-
-    public static void main(String[] args) throws Exception {
-       
-    }
-
-
-    
-    public JSONObject translate(Object obj) throws Exception {
-        String str = com.alibaba.fastjson.JSON.toJSONString(obj);
-        System.err.println(str);
-        String returnStr = HanLP.convertToTraditionalChinese((String)str );
-        JSONObject jsonobject = JSONObject.parseObject(returnStr);
-        return jsonobject;
-  }
 
 }
